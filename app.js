4d54f50c32f5c9a0bb2fc1cf5cff12518a611a86
Javier <immortal_971@live.com.mx>
2019-02-09 13:40:09 -0600

    creacion de vista detalle

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var detalleRouter = require('./routes/detalle');
 
 var app = express();
 
@@ -21,10 +22,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/detalle', detalleRouter);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+app.use(function(request,res){
+
+  res.status(404);
+  res.render('errors/404.pug',{detalle:'Upss...'});
+
+});
+
+app.use(function(){
+
+  res.status(500);
+  res.render('errors/500.pug',{detalle:'Valio madre ...'});
+
 });
 
 // error handler
